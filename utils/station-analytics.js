b945a954c11c457261e6d48e57ba1886960f5ac7
edibotopic <edibotopic@gmail.com>
2024-08-19 21:10:05 +0100

    Basic analytics implementation

---
+++ utils/station-analytics.js
@@ -0,0 +1,22 @@
+export const stationAnalytics = {
+  getAve(station) {
+    let sum = 0;
+    if (station.reports.length > 0) {
+      for (let i = 0; i < station.reports.length; i++) {
+        sum += array[i];
+        }
+      }
+    }
+    return shortestTrack;
+  },
+
+  // QUERY: needless?
+  getMax(array) {
+    Math.max(array);
+  },
+
+  getMin(array) {
+    Math.max(array);
+  },
+
+};
