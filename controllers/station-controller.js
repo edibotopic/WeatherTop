08eea0da14b4e9d8a9ee828712f13b0c8c7b7e57
edibotopic <edibotopic@gmail.com>
2024-08-22 15:25:36 +0100

    implement reports summary and min-max data analysis

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -1,12 +1,28 @@
 import { stationStore } from '../models/station-store.js'
 import { reportsStore } from '../models/reports-store.js'
+import { stationAnalytics } from "../utils/station-analytics.js";
 
 export const stationController = {
   async index(request, response) {
     const station = await stationStore.getStationById(request.params.id)
+    const reports = await reportsStore.getReportsByStationId(request.params.id)
+
+    let temp_max = stationAnalytics.getMax(reports, "temp");
+    let temp_min = stationAnalytics.getMin(reports, "temp");
+    let wind_max = stationAnalytics.getMax(reports, "wind_speed");
+    let wind_min = stationAnalytics.getMin(reports, "wind_speed");
+    let pressure_max = stationAnalytics.getMax(reports, "pressure");
+    let pressure_min = stationAnalytics.getMin(reports, "pressure");
+
     const viewData = {
       title: "Station",
       station: station,
+      temp_max: temp_max,
+      temp_min: temp_min,
+      wind_max: wind_max,
+      wind_min: wind_min,
+      pressure_max: pressure_max,
+      pressure_min: pressure_min,
     }
     response.render("station-view", viewData)
   },
@@ -23,6 +39,9 @@ export const stationController = {
     console.log(`adding report ${newReport.title}`)
     await reportsStore.addReport(station._id, newReport)
     response.redirect("/station/" + station._id)
+
   }
+
+  //NOTE: do I need a getSummary method?
 }
 
